025f3ecf4162ffe7dcf38123dd54742581309af2
Jorge Jara <jorgejarainostroza@gmail.com>
2022-07-22 16:05:02 -0400

    Configure GraphQL client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,22 @@
+import { ApolloProvider } from '@apollo/client';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import type { AppProps } from 'next/app';
 
 import Layout from '@/components/Layout';
 
+import apolloClient from '@/lib/apollo';
+
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </UserProvider>
+    <ApolloProvider client={apolloClient}>
+      <UserProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </UserProvider>
+    </ApolloProvider>
   );
 }
 
