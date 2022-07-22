e002d7894b4668d7a79a9489520e18af7130b296
Jorge Jara <jorgejarainostroza@gmail.com>
2022-07-21 20:26:42 -0400

    Install @auth0/nextjs-auth0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
+import { UserProvider } from '@auth0/nextjs-auth0';
 import type { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className="w-screen h-screen">
-      <Component {...pageProps} />
-    </main>
+    <UserProvider>
+      <main className="w-screen h-screen">
+        <Component {...pageProps} />
+      </main>
+    </UserProvider>
   );
 }
 
